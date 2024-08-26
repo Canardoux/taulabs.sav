7830b2a614fbcb2e6e5807076db1e0701f762f01
larpoux <larpoux@Mac-mini.local>
2024-08-26 10:16:15 +0200

    Init

---
+++ example/android/app/src/main/kotlin/canardoux/xyz/taulabs_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package canardoux.xyz.taulabs_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ ios/Classes/taulabs.c
@@ -0,0 +1,3 @@
+// Relative import to be able to reuse the C sources.
+// See the comment in ../taulabs.podspec for more information.
+#include "../../src/taulabs.c"
